4aaa522e7ecfe302d74c8dc8a3299d603c9c2801
Tim Jacobs <tlpjacobs@tudelft.nl>
2026-05-06 09:02:04 +0200

    Wanna pull as well

---
+++ Structures and Materials/X-frame.py
@@ -0,0 +1,5 @@
+# X-frame will be modelled as two arms (simplified as beams) crossing eachother. Forces are applied at tips and deconstructed into components parallel and perpendicular to the arms.
+import math
+import numpy as np
+
+L = 0.03 # m
