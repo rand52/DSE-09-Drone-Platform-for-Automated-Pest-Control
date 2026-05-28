993ff78eb2051c2002caa1967ae9c160c583df70
Bramb05 <bbaarda@tudelft.nl>
2026-05-28 14:26:55 +0200

    added quaternian multiplyer and euler to quaternian rates converter

--- Control and Integration/controltools.py
+++ Control and Integration/controltools.py
@@ -14,9 +14,28 @@ def euler_to_quat(phi, theta, psi):
          [(s_phi*s_theta*c_psi)-(c_phi*s_psi), (s_phi*s_theta*s_psi)+(c_phi*c_psi), s_phi*c_theta],
          [(c_phi*s_theta*c_psi)+(s_phi*s_psi), (c_phi*s_theta*s_psi)-(s_phi*c_psi), c_phi*c_theta]]
     h = R.from_matrix(H)
-    print(h.as_quat())
 
     return h.as_quat()
 
-R = euler_to_quat(1, 1, 1)
+# angular rate should be a vector containing [roll rate, pitch rate, yaw rate] q should be the quaternion
+def rates_euler_to_quat(angular_rate, q):
+    # cross_vec = [0]
+    # cross_vec.append(angular_rate)
+    print(([0] + angular_rate))
+    q_dot = 0.5 * quat_multiply(q, [0]+ angular_rate)
+    return q_dot
+
+def quat_multiply(p, q):
+    p0, p1, p2, p3 = p
+    q0, q1, q2, q3 = q
+    t0 = q0*p0 - q1*p1 - q2*p2 - q3*p3
+    t1 = q0*p1 + q1*p0 - q2*p3 + q3*p2
+    t2 = q0*p2 + q1*p3 + q2*p0 - q3*p1
+    t3 = q0*p3 - q1*p2 + q2*p1 + q3*p0
+    return np.array([t0, t1, t2, t3])
+
+j = euler_to_quat(np.pi/2, 0, -np.pi/2)
+print(j)
+rates= rates_euler_to_quat([2.0, 3.0, 3.0], j)
+print(rates)
 
