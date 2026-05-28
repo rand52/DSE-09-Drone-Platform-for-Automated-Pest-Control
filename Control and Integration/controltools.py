fixed to scalar-first quaternion

Control and Integration/controltools.py
@@ -1,22 +1,13 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
+import scipy.spatial.transform
 
 #Coordinate frame transformations:
 # Uses numpy so should be given in radians
-def euler_to_quat(phi, theta, psi):
-    c_phi = np.cos(phi)
-    s_phi = np.sin(phi)
-    c_theta = np.cos(theta)
-    s_theta = np.sin(theta)
-    c_psi = np.cos(psi)
-    s_psi = np.sin(psi)
-    H = [[c_theta*c_psi, c_theta*s_psi, -s_theta],
-         [(s_phi*s_theta*c_psi)-(c_phi*s_psi), (s_phi*s_theta*s_psi)+(c_phi*c_psi), s_phi*c_theta],
-         [(c_phi*s_theta*c_psi)+(s_phi*s_psi), (c_phi*s_theta*s_psi)-(s_phi*c_psi), c_phi*c_theta]]
-    h = R.from_matrix(H)
-
-    return h.as_quat()
-
+def euler_to_quat(psi, theta, phi):
+    i = R.from_euler('zyx', [psi, theta, phi], degrees=False)
+    x, y, z, w = i.as_quat()
+    return np.array([w, x, y, z])
 # angular rate should be a vector containing [roll rate, pitch rate, yaw rate] q should be the quaternion
 def rates_euler_to_quat(angular_rate, q):
     # cross_vec = [0]
@@ -35,7 +26,7 @@ def quat_multiply(p, q):
     return np.array([t0, t1, t2, t3])
 
 j = euler_to_quat(np.pi/2, 0, -np.pi/2)
-print(j)
+print('after switch', j)
 rates= rates_euler_to_quat([2.0, 3.0, 3.0], j)
 print(rates)
 